bf9565bad700a6d2c7300510790723905a55a2dd
heyrhythm <121548218+heyrhythm@users.noreply.github.com>
2025-03-31 22:38:04 +0530

    app.jsx,css,index.css,navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,9 +4,10 @@ import "./index.css";
 import Home from "./Pages/Home";
 import Cart from "./Pages/Cart";
 import PlaceOrder from "./Pages/PlaceOrder";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
-import LogIn from "./Pages/LogIn";
+
+
 import Menu from "./Pages/Menu";
 import Navbar from "./Components/Navbar"; // Import the Navbar component
 import Footer from "./Components/Footer";
@@ -18,7 +19,7 @@ import Dashboard from "./Pages/Dashboard";
 
 const App = () => {
   return (
-      <div>
+      <div className="consistent-scrollbar">
         <Navbar />
         
         <div className="w-[80%] mx-auto"> {/* Center the content */}
@@ -29,29 +30,14 @@ const App = () => {
             <Route path="/search" element={<Searchicon />} />
             <Route path="/mobileapp" element={<MobileApp />} />
             <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/order" element={<PlaceOrder />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<LogIn />} />
+            <Route path="/users" element={<UserList />}/>
+            
           </Routes>
         </div>
-    <div className=" ">
-      <BrowserRouter>
-        {/* <Navbar /> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/mobileapp" element={<MobileApp />} />
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/order" element={<PlaceOrder />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/users" element={<UserList />}/>
-          <Route path="/login" element={<LogIn />} />
-        </Routes>
-        <Footer className="w-screen" />
-        </BrowserRouter>
-        </div>
+    <Footer/>
     </div>
   );
 };
